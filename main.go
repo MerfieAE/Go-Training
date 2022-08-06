686fa8b189dd5ae64d1dcec51992b3281bf3df9e
Victor <victorbejenar@yandex.ru>
2022-08-06 22:54:40 +0300

    taskThree

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "tasks/tasks"
 func main() {
 	tasks.TaskOne()
 	tasks.TaskTwo()
+	tasks.TaskThree()
 }
--- tasks/task3.go
+++ tasks/task3.go
@@ -1,1 +1,28 @@
 package tasks
+
+import (
+	"fmt"
+	"sync"
+)
+
+func squareSum(arr int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var sum int
+	for i := 1; i <= arr; i++ {
+		if i%2 == 0 {
+			sum += i * i
+		}
+	}
+	fmt.Println(sum)
+}
+
+func TaskThree() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go squareSum(10, &wg)
+
+	fmt.Println("Waiting for goroutines to finish...")
+	wg.Wait()
+	fmt.Println("Done!")
+}
