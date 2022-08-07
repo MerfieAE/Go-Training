78dc9ec1297d26b18c838b8076eabb4c19375bd5
Victor <victorbejenar@yandex.ru>
2022-08-07 12:28:38 +0300

    taskSixteen

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "tasks/tasks"
 
 func main() {
-	//tasks.TaskOne()
-	//tasks.TaskTwo()
-	//tasks.TaskThree()
-	//tasks.TaskFive()
+	tasks.TaskOne()
+	tasks.TaskTwo()
+	tasks.TaskThree()
+	tasks.TaskFive()
 	tasks.TaskSeventeen([]int{1, 3, 5, 8, 15}, 8)
+	tasks.TaskSixteen([]int{83, 32, 4, 7, 1, 3, 5, 8, 15})
 }
