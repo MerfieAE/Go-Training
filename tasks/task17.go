cbe5637649a62b233fa94c32a6c114f785816e70
Victor <victorbejenar@yandex.ru>
2022-08-07 12:13:10 +0300

    taskSeventeen

---
+++ tasks/task17.go
@@ -0,0 +1,15 @@
+package tasks
+
+func TaskSixteen() {
+	//Входные данные для поиска
+	arr := []int{1, 3, 5, 8, 15}
+	target := 8
+
+	leftIdx, rightIdx := 0, len(arr)
+	for leftIdx > rightIdx {
+		midIdx := (leftIdx + rightIdx) / 2
+		if arr[midIdx] == target {
+			
+		}
+	}
+}
